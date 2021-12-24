16c0af0ec0770471c34a367b3fb76ea0d7e8197d
johniscoolboy <96302736+johniscoolboy@users.noreply.github.com>
2021-12-24 10:40:24 +0000

    Add files via upload

--- configure.sh
+++ configure.sh
@@ -3,10 +3,9 @@
 # Download and install V2Ray
 config_path=$PROTOCOL"_ws_tls.json"
 mkdir /tmp/v2ray
-curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
-install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
-install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+install -m 755 /tmp/v2ray/xray /usr/local/bin/xray
 # Remove temporary directory
 rm -rf /tmp/v2ray
 # V2Ray new configuration
@@ -18,6 +17,6 @@ cd /opt/test
 dd if=/dev/zero of=100mb.bin bs=100M count=1
 dd if=/dev/zero of=10mb.bin bs=10M count=1
 # Run V2Ray
-/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json &
+/usr/local/bin/xray -config /usr/local/etc/v2ray/config.json &
 # Run nginx
 /bin/bash -c "envsubst '\$PORT,\$WS_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
